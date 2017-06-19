1da0432c044a98ea5e38737e52ed4cd5143459a1
Chenshiwen <cswzmg@live.com>
2017-06-19 11:49:04 +0800

    行为动画

--- SceneKit-05/ViewController.m
+++ SceneKit-05/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
+
+#import <SceneKit/SceneKit.h>
+
 @interface ViewController ()
+
+@property (nonatomic,strong) SCNView *mScnView;
 
 @end
 
@@ -16,10 +21,59 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    SCNScene *scene = [SCNScene scene];
+    self.mScnView.scene = scene;
+    [self.view addSubview:self.mScnView];
+//    添加盒子
+    [self addBox];
+//    添加相机
+    [self addCamera];
+
+}
+-(void)addBox{
+    SCNBox *box = [SCNBox boxWithWidth:10 height:10 length:10 chamferRadius:0];
+    box.firstMaterial.diffuse.contents = [UIImage imageNamed:@"box2.jpg"];
+    SCNNode *boxNode = [SCNNode nodeWithGeometry:box];
+    [self.mScnView.scene.rootNode addChildNode:boxNode];
+    [boxNode runAction:[self addAction]];
+}
+-(void)addCamera{
+    SCNCamera *camera = [SCNCamera camera];
+    SCNNode *cameraNode = [SCNNode node];
+    cameraNode.camera = camera;
+    cameraNode.position = SCNVector3Make(0, 0, 50);
+    [self.mScnView.scene.rootNode addChildNode:cameraNode];
 }
 
+-(SCNView *)mScnView{
+    if (!_mScnView) {
+        _mScnView = [[SCNView alloc] initWithFrame:self.view.bounds];
+        _mScnView.backgroundColor = [UIColor blackColor];
+        _mScnView.center = self.view.center;
+        _mScnView.allowsCameraControl = YES;
+        
+    }
+    return _mScnView;
+}
 
+-(SCNAction*)addAction{
+//    X轴转
+    SCNAction *rotationX = [SCNAction rotateByAngle:-2*M_PI aroundAxis:SCNVector3Make(1, 0, 0) duration:2];
+//    Y轴转
+    SCNAction *rotationY = [SCNAction rotateByAngle:2*M_PI aroundAxis:SCNVector3Make(0, 1, 0) duration:2];
+//    设置旋转顺序
+    SCNAction *rotateSequence = [SCNAction sequence:@[rotationX,rotationY]];
+    
+    
+    SCNAction *moveUP = [SCNAction moveTo:SCNVector3Make(0, 20, 0) duration:2];
+    SCNAction *moveDown = [SCNAction moveTo:SCNVector3Make(0, -20, 0) duration:2];
+//    设置移动顺序
+    SCNAction *moveSequence = [SCNAction sequence:@[moveUP,moveDown]];
+//    组合动作 上移x轴转动，下移y轴转动
+    SCNAction *group = [SCNAction group:@[moveSequence,rotateSequence]];
+    return [SCNAction repeatActionForever:group];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
